2801a9a32060399ec56a44839dfeb4db395f68e7
Sam Robertson <samrobertson5@outlook.com>
2025-11-15 15:47:18 +0000

    Colour palette for customisation
    added the class for colorpallete. added some palette examples

---
+++ app_geek_hobby_app/lib/Classes/color_palette.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+class ColorPalette {
+  Color primaryColor;
+  Color primaryVariant;
+  Color secondaryColor;
+  Color backgroundColor;
+  Color surfaceColor;
+  Color accentColor;
+
+  ColorPalette({
+    required this.primaryColor,
+    required this.primaryVariant,
+    required this.secondaryColor,
+    required this.backgroundColor,
+    required this.surfaceColor,
+    required this.accentColor,
+  });
+
+  // Example: default palette
+  static ColorPalette defaultPalette = ColorPalette(
+    primaryColor: Color(0xFF6200EE),
+    primaryVariant: Color(0xFF3700B3),
+    secondaryColor: Color(0xFF03DAC6),
+    backgroundColor: Color(0xFFF5F5F5),
+    surfaceColor: Color(0xFFFFFFFF),
+    accentColor: Color(0xFFFF5722),
+  );
+}
---
+++ app_geek_hobby_app/lib/Data/color_schemes.dart
@@ -0,0 +1,93 @@
+import 'package:flutter/material.dart';
+import 'package:app_geek_hobby_app/Classes/color_palette.dart';
+
+// Your existing palette
+ColorPalette palette1 = ColorPalette(
+  primaryColor: Color(0xFF1A2730),
+  primaryVariantColor: Color(0xFF424048),
+  secondaryColor: Color(0xFF45586C),
+  backgroundColor: Color(0xFFA63E1B),
+  surfaceColor: Color(0xFFB0CEE2),
+  accentColor: Color(0xFFE95D2C),
+);
+
+// Palette 2
+ColorPalette palette2 = ColorPalette(
+  primaryColor: Color(0xFF2F2B1F),
+  primaryVariantColor: Color(0xFF344932),
+  secondaryColor: Color(0xFF4B4436),
+  backgroundColor: Color(0xFF9D8E62),
+  surfaceColor: Color(0xFFE5D013),
+  accentColor: Color(0xFFEEDDEE),
+);
+
+// Palette 3
+ColorPalette palette3 = ColorPalette(
+  primaryColor: Color(0xFF02485A),
+  primaryVariantColor: Color(0xFF152C44),
+  secondaryColor: Color(0xFF26E5D3),
+  backgroundColor: Color(0xFFD5A29D),
+  surfaceColor: Color(0xFFDB5365),
+  accentColor: Color(0xFFFD7767),
+);
+
+// Palette 4
+ColorPalette palette4 = ColorPalette(
+  primaryColor: Color(0xFF323429),
+  primaryVariantColor: Color(0xFF594535),
+  secondaryColor: Color(0xFF5B5D5A),
+  backgroundColor: Color(0xFF95704D),
+  surfaceColor: Color(0xFFCCDDDD),
+  accentColor: Color(0xFFD4B18F),
+);
+
+// Palette 5
+ColorPalette palette5 = ColorPalette(
+  primaryColor: Color(0xFF40E9D6),
+  primaryVariantColor: Color(0xFF4B4051),
+  secondaryColor: Color(0xFF814B42),
+  backgroundColor: Color(0xFFCC9E7C),
+  surfaceColor: Color(0xFFEBD8B6),
+  accentColor: Color(0xFFF6A46D),
+);
+
+// Palette 6
+ColorPalette palette6 = ColorPalette(
+  primaryColor: Color(0xFF0F1221),
+  primaryVariantColor: Color(0xFF39455D),
+  secondaryColor: Color(0xFFA99BAE),
+  backgroundColor: Color(0xFFD9B9BD),
+  surfaceColor: Color(0xFFE39A78),
+  accentColor: Color(0xFFFFF6EC),
+);
+
+// Palette 7
+ColorPalette palette7 = ColorPalette(
+  primaryColor: Color(0xFF132C47),
+  primaryVariantColor: Color(0xFF294C63),
+  secondaryColor: Color(0xFF78AEAD),
+  backgroundColor: Color(0xFFD4ECE9),
+  surfaceColor: Color(0xFFD86350),
+  accentColor: Color(0xFFFBAE54),
+);
+
+// Palette 8
+ColorPalette palette8 = ColorPalette(
+  primaryColor: Color(0xFF1B0C1A),
+  primaryVariantColor: Color(0xFF2D222F),
+  secondaryColor: Color(0xFF4B2138),
+  backgroundColor: Color(0xFF6D3C52),
+  surfaceColor: Color(0xFF765D67),
+  accentColor: Color(0xFFFADCD5),
+);
+
+// Palette 9
+ColorPalette palette9 = ColorPalette(
+  primaryColor: Color(0xFF116871),
+  primaryVariantColor: Color(0xFF264148),
+  secondaryColor: Color(0xFF7C5E57),
+  backgroundColor: Color(0xFF8C7970),
+  surfaceColor: Color(0xFF99D0A8),
+  accentColor: Color(0xFFEFDFC4),
+);
+
